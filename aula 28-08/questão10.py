1e2820e1a026ca798f0cdd99e46bede9b5fd346e
Lipescotti <142691835+Lipescotti@users.noreply.github.com>
2025-08-28 20:42:16 -0400

    Update questão10.py

--- aula 28-08/questão10.py
+++ aula 28-08/questão10.py
@@ -1,12 +1,18 @@
 altura = float(input("Digite sua altura: "))
 sexo = int(input("Digite 1 para masculino e 2 para feminino: "))
 
+
 if sexo == 1:
     ideal = (72.7 * altura) - 58
-    print("Seu peso ideal é :", ideal)
+
 elif sexo == 2:
-    ideal = (62.1 * altura) - 44,7
-    print("Seu peso ideal é :", ideal)
-else:
-    print("\nEntrada invalida\n")
+    ideal = (62.1 * altura) - 44.7
     
+else:
+    ideal = "invalido"
+    print("\nEntrada invalida\n")
+
+
+if ideal != "invalido":
+    ideal = round(ideal, 3)
+    print("Seu peso ideal = ", ideal, " kg")
